900cd4613febeb7d70d476cbe9dcb5dd5ea8f99c
blackdotsh <blackice@builds>
2014-08-06 13:40:51 -0500

    removed unreachable test node

--- speedtest.sh
+++ speedtest.sh
@@ -109,18 +109,9 @@ speedtest 192.210.229.206;
 echo "Speedtest from Beauharnois, Quebec, Canada [ generously donated by http://mycustomhosting.net ] on a shared 1000 Mbps port in / 500 Mbps port out";
 speedtest 198.50.209.250;
 
-## Buffalo, NY, USA
-echo "Speedtest from Buffalo, NY, USA on a shared 1 Gpbs port":
-speedtest 23.94.28.158;
-
 ## Atlanta, GA, USA (donated by  http://hostus.us)
 echo "Speedtest from Atlanta, GA, USA [ generously donated by http://hostus.us ] on a shared 1 Gbps port";
 speedtest 162.245.216.241;
-
-#server is down
-## Clifton, NJ, USA (donated by  http://dedicatedminds.com)
-#echo "Speedtest from Clifton, NJ, USA [ generously donated by http://dedicatedminds.com ] on a shared 1 Gbps port";
-#speedtest 199.36.221.36;
 
 ##Jacksonville, FL, USA (donated by http://maximumvps.net)
 echo "Speedtest from Jacksonville, FL, USA [ generously donated by http://maximumvps.net ] on a shared 1 Gbps port";
